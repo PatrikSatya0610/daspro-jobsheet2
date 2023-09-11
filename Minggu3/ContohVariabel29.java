220ebbb6243104621a3baf8c6585899c7587dd0a
Patrik Satya <pspr0621@gmail.com>
2023-09-11 13:54:53 +0700

    Variabel

--- Minggu3/ContohVariabel29.java
+++ Minggu3/ContohVariabel29.java
@@ -1,15 +1,15 @@
 package Minggu3;
-public class ContohVariabel29 {
 
+public class ContohVariabel29 {
     public static void main(String []args){
 
-        String salahsatuhobysayaadalah = "Bermain petak umpet";
+        String Salah Satu Hoby Saya Adalah = "Bermain petak umpet";
         boolean ispandai = true;
         char jeniskelamin = 'L';
         byte umursekarang = 20;
         double $ipk = 3.24, tinggi = 1.78;
 
-        System.out.println("salahsatuhobysayaadalah");
+        System.out.println("Salah Satu Hoby Saya Adalah");
         System.out.println("Apakah pandai?"+ispandai);
         System.out.println("Jenis Kelamin"+jeniskelamin);
         System.out.println("Umurku saat ini:"+jeniskelamin);
