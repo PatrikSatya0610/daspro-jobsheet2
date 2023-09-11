3aceeadbd79ded0b3646f6b7a5e7a0f932478025
Patrik Satya <pspr0621@gmail.com>
2023-09-11 14:51:32 +0700

    ahahaha

--- Minggu3/ContohVariabel29.java
+++ Minggu3/ContohVariabel29.java
@@ -9,10 +9,10 @@ public class ContohVariabel29 {
         byte umursekarang = 20;
         double $ipk = 3.24, tinggi = 1.78;
 
-        System.out.println("salahsatuhobysayaadalah");
+        System.out.println(salahsatuhobysayaadalah);
         System.out.println("Apakah pandai?"+ispandai);
         System.out.println("Jenis Kelamin"+jeniskelamin);
-        System.out.println("Umurku saat ini:"+jeniskelamin);
+        System.out.println("Umurku saat ini:"+umursekarang);
         System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s",$ipk, tinggi));
     }
 }
