84914dd2d9173c981a789ad88db12715f9ad513b
Patrik Satya <pspr0621@gmail.com>
2023-09-11 14:13:54 +0700

    tipe data

--- Minggu3/ContohTipeData29.java
+++ Minggu3/ContohTipeData29.java
@@ -11,13 +11,13 @@ public class ContohTipeData29 {
         long saldo = 150000000;
         int angkaDesimal = 0x10;
 
-        System.out.println("Golongan darah\t\t\t\t\t\t: " + (byte) golonganDarah);
+        System.out.println("Golongan darah\t\t\t\t\t\t\t: " + (byte) golonganDarah);
         System.out.println("Jarak\t\t\t\t\t\t\t\t: " + jarak);
-        System.out.println("Jumlah penduduk dalam satu dusun\t: " + jumlahPendudukDalamSatuDusun);
+        System.out.println("Jumlah penduduk dalam satu dusun\t\t\t: " + jumlahPendudukDalamSatuDusun);
         System.out.println("Suhu\t\t\t\t\t\t\t\t: " + suhu);
         System.out.println("Berat\t\t\t\t\t\t\t\t: " + berat);
         System.out.println("Saldo\t\t\t\t\t\t\t\t: " + saldo);
-        System.out.println("Angka Desimal\t\t\t\t\t\t: " + angkaDesimal);
+        System.out.println("Angka Desimal\t\t\t\t\t\t\t: " + angkaDesimal);
 
     }
 }
