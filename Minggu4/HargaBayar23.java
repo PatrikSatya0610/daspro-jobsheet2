2006cbd3f6234c6e5676a153e6b3d5b05847415d
Patrik Satya <pspr0621@gmail.com>
2023-09-18 12:43:04 +0700

    bayar

---
+++ Minggu4/HargaBayar23.java
@@ -0,0 +1,11 @@
+package Minggu4;
+
+import java.util.Scanner;
+
+public class HargaBayar23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+    
+}
