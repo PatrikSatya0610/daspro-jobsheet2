f6aaa5b9da5ca9dc247533f708dbbab17b4afe0e
Patrik Satya <pspr0621@gmail.com>
2023-10-02 15:17:32 +0700

    minggu6

---
+++ Minggu6/Pemilihan2Percobaan1_23.java
@@ -0,0 +1,21 @@
+package Minggu6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_23 {
+     public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+        System.out.println("Masukkan Tahun");
+        int tahun = input17.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if (tahun % 100 != 0)
+                System.out.println("Tahun Kabisat");
+            else if (tahun % 100 == 0)
+                if (tahun % 400 == 0)
+                    System.out.println("Tahun Kabisat");
+            else
+                System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
