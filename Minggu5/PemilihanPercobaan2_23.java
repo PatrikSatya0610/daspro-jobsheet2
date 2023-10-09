9fc3ce95f3fe95e8ae733102b3946f65c7b8f98e
Patrik Satya <pspr0621@gmail.com>
2023-10-09 13:39:30 +0700

    percobaan2

--- Minggu5/PemilihanPercobaan2_23.java
+++ Minggu5/PemilihanPercobaan2_23.java
@@ -6,19 +6,42 @@ public class PemilihanPercobaan2_23 {
     public static void main(String[] args) {
         Scanner input23 = new Scanner(System.in);
 
-        System.out.print("Nilai uas     : ");
-        float uas=input23.nextFloat();
-        System.out.print("Nilai uts     : ");
-        float uts=input23.nextFloat();
-        System.out.print("Nilai kuis    : ");
-        float kuis=input23.nextFloat();
-        System.out.print("Nilai tugas   : ");
-        float tugas=input23.nextFloat();
+        System.out.print("Nilai UAS : ");
+        float uas = input23.nextFloat();
+        System.out.print("Nilai UTS : ");
+        float uts = input23.nextFloat();
+        System.out.print("Nilai Kuis : ");
+        float kuis = input23.nextFloat();
+        System.out.print("Nilai Tugas : ");
+        float tugas = input23.nextFloat();
+        String predikat;
 
-        float total=(uas*0.4f)+(uts*0.3f)+(kuis*0.1f)+(tugas*0.2f);
-        String message= total<65 ? "remidi" : "tidak remidi";
+        float total = (uas * 0.4F)+(uts * 0.4F)+(kuis * 0.1F)+(tugas * 0.2F);
 
-        System.out.println("Nilai akhir "+total+" sehingga "+message);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+
+        if (total > 80 && total <= 100){
+            predikat = "A";
+        }
+        else if (total > 73 && total <= 80){
+            predikat = "B+";
+        }
+        else if (total > 65 && total <= 73){
+            predikat = "B";
+        }
+        else if (total > 60 && total <= 65){
+            predikat = "C+";
+        }
+        else if (total > 50 && total <= 60){
+            predikat = "C";
+        }
+        else if (total > 39 && total <= 50){
+            predikat = "D";
+        }
+        else{predikat = "E";
+        }
+        
+        System.out.println("Nilai Akhir = " + total + " dengan predikat " + predikat + " sehingga " + message);
     }
     
 }
